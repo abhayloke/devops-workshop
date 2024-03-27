ea9688a038360b7e5d68ae0bcdba1b2f2d09ba62
abhayloke <abhay.loke@gmail.com>
2024-03-27 18:48:23 +0530

    V4 for Multiple instance

--- terraform_code/V4-EC2 with VPC_for each.tf
+++ terraform_code/V4-EC2 with VPC_for each.tf
@@ -3,12 +3,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-0c101f26f147fa7fd"
+    ami = "ami-080e1f13689e07408"
     instance_type = "t2.micro"
     key_name = "key4"
     //security_groups = ["demo-sg"]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpw-public-subnet-01.id
+for_each = toset(["jenkins-master", "build-slave","ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
+
 
 }
 
